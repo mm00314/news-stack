88f1b7633037e94e1ae4516aa705dc42cd8adb20
mm00314 <mm00314@ewhain.net>
2019-11-28 14:15:26 +0900

    Update app.js

--- app.js
+++ app.js
@@ -20,7 +20,7 @@ var mysql = require('mysql');
 var conn = mysql.createConnection({
   host     : 'localhost',
   user     : 'root',
-  password : 'mysql1234',
+  password : '098098poipoi-',
   database : 'news_stack',
   port: 3306  /* 8889 on Mac client */
 });
